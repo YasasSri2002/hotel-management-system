a60ba4ad46f480c987f28cef2d165503e25139f3
YasasSri2002 <srimedagedara234@gmail.com>
2025-06-08 13:59:37 +0530

    Updated EmployeeEntity and employe DTO added

--- src/main/java/com/yasas/sunHotel/entity/EmployeeEntity.java
+++ src/main/java/com/yasas/sunHotel/entity/EmployeeEntity.java
@@ -1,7 +1,6 @@
 package com.yasas.sunHotel.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,4 +11,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer serviceId;
+
+    private String name;
+
+
+
 }
