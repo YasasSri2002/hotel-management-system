a9cf93aa5484cef20b91fcd8ef8c49e00de910bc
YasasSri2002 <srimedagedara234@gmail.com>
2025-06-08 13:48:46 +0530

    EmployeeEntity and employe DTO added

--- src/main/java/com/yasas/sunHotel/entity/Employee.java
+++
@@ -1,9 +0,0 @@
-package com.yasas.sunHotel.entity;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name="employee")
-public class Employee {
-}
---
+++ src/main/java/com/yasas/sunHotel/entity/EmployeeEntity.java
@@ -0,0 +1,15 @@
+package com.yasas.sunHotel.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="employee")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeEntity {
+}
